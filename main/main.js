059f82279e0754bdc98cc7793fafc5a7ba705b5b
cao <caoxi324@foxmail.com>
2018-09-25 23:12:18 +0800

    javascript3

--- main/main.js
+++ main/main.js
@@ -1,3 +1,11 @@
 module.exports = function main() {
+	var str = '28465591';
+	var a = 0;
+	while(str > 10) {
+		a = a + str%10;
+		str = parseInt(str/10);
+	}
+	a = a + str;
+	console.log(a);
   // Write your code here
 };
